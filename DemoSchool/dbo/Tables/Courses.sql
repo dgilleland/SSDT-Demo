15c86feeab70ec4dfc0e36823bead9710fad1337
Dan Gilleland <dgilleland@nait.ca>
2020-12-16 08:34:56 -0700

    Added more tables

---
+++ DemoSchool/dbo/Tables/Courses.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Courses]
+(
+    [CourseId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Number] CHAR(9) NOT NULL, 
+    [Name] VARCHAR(50) NULL, 
+    CONSTRAINT [CK_Courses_Number] CHECK ([Number] LIKE '[A-Z][A-Z][A-Z][A-Z]-[0-9][0-9][0-9][0-9]')
+)
---
+++ DemoSchool/dbo/Tables/Registrations.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Registrations]
+(
+    [CourseId] INT NOT NULL , 
+    [StudentId] INT NOT NULL, 
+    [FinalMark] TINYINT NULL, 
+    PRIMARY KEY ([CourseId], [StudentId]), 
+    CONSTRAINT [FK_Registrations_Courses] FOREIGN KEY ([CourseId]) REFERENCES [Courses]([CourseId]), 
+    CONSTRAINT [FK_Registrations_Students] FOREIGN KEY ([StudentId]) REFERENCES [Students]([StudentId])
+)
